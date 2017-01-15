505d90074e8a78a675d3c0b1351c209ee4758930
Michael Paladino <michael@revunit.com>
2017-01-14 23:41:02 -0600

    More ActiveAdmin cleanup

--- app/admin/vocabulary_list.rb
+++ app/admin/vocabulary_list.rb
@@ -3,4 +3,14 @@ ActiveAdmin.register VocabularyList do
 
   menu parent: "Vocabulary", priority: 1
 
+  index do
+    selectable_column
+    id_column
+    column :name
+    column "Words" do |vlist|
+      link_to "View Words", :controller => "vocabulary_words", :action => "index", 'q[vocabulary_list_id_eq]' => "#{vlist.id}".html_safe
+    end
+    actions
+end
+
 end
--- app/admin/vocabulary_word.rb
+++ app/admin/vocabulary_word.rb
@@ -3,4 +3,20 @@ ActiveAdmin.register VocabularyWord do
 
   menu parent: "Vocabulary", priority: 1
 
+  controller do
+    def scoped_collection
+      super.includes :vocabulary_list
+    end
+  end
+
+  index do
+     selectable_column
+     id_column
+     column :word
+     column :vocabulary_list, sortable: 'vocabulary_lists.name'
+     column :created_at
+     column :updated_at
+     actions
+   end
+
 end
